f33735abb1dcb4730aea8e569114a47f56d9c359
halcyon-jac-k <e135712@Yoshihiro-no-MacBook-Pro.local>
2015-06-22 15:28:49 +0900

    hoge

---
+++ Jack/SpeedTest/rxeflash.sh
@@ -0,0 +1,4 @@
+echo Executing NeXTTool to upload benchmark.rxe...
+ /Users/e135712/etrobo/nxtOSEK/nexttool /COM=usb -download=benchmark.rxe
+ /Users/e135712/etrobo/nxtOSEK/nexttool /COM=usb -listfiles=benchmark.rxe
+echo NeXTTool is terminated.
