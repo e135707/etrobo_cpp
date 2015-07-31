6a756b33d9bbf33df93ecc5992ee32026abd2192
Ryusuke1015 <e135733@ie.u-ryukyu.ac.jp>
2015-07-31 12:16:31 +0900

    PID

---
+++ suzuki/pid_test/build/syscalls.d
@@ -0,0 +1,22 @@
+build/syscalls.o: ../../../ecrobot/../ecrobot/c/syscalls.c \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/stdlib.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/machine/ieeefp.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/_ansi.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/newlib.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/sys/config.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/sys/features.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/include/stddef.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/sys/reent.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/_ansi.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/sys/_types.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/machine/_types.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/machine/_default_types.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/sys/lock.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/machine/stdlib.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/alloca.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/reent.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/machine/types.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/sys/stat.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/time.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/machine/time.h \
+ /Users/e135733/etrobo/yagarto-4.6.2/bin/../lib/gcc/arm-none-eabi/4.6.2/../../../../arm-none-eabi/sys-include/sys/types.h
---
+++ suzuki/pid_test/test_move.cpp
@@ -0,0 +1,157 @@
+/* test_move*/
+/*
+メモ
+motorAは+で右回り-で左回り
+motorB,Cは-で直進
+motorB = 左
+motorC = 右
+6/23
+右旋回はほぼ完璧だが左旋回に少し難あり
+*/
+
+// ECRobot++ API
+#include "LightSensor.h"
+#include "SonarSensor.h"
+#include "SoundSensor.h"
+#include "TouchSensor.h"
+#include "GyroSensor.h"
+#include "Clock.h"
+#include "Lcd.h"
+
+#include "Motor.h"
+#include "Nxt.h"
+
+using namespace ecrobot;
+
+extern "C"
+{
+#include "kernel.h"
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+#include"ecrobot_types.h"
+
+LightSensor  light(PORT_3);
+SonarSensor  sonar(PORT_4);
+GyroSensor   gyro(PORT_1);
+TouchSensor  touch(PORT_2);
+
+Motor       motorA(PORT_A);
+Motor       motorB(PORT_B);
+Motor       motorC(PORT_C);
+
+/* nxtOSEK hook to be invoked from an ISR in category 2 */
+void user_1ms_isr_type2(void)
+{
+  SleeperMonitor(); // needed for I2C device and Clock classes
+}
+
+static float pid_sample(float sensor_val);
+static float math_limit(float pid_val, float pid_min, float pid_max);
+
+float DELTA_T=0.005;
+float KP=0.01;
+float KI=0.01;
+float KD=0.01;
+float target_val=380; //光センサの目標値
+
+int threshold=380;
+
+static float diff[2]; //"S32" 32bitのsinged(符号あり)整数型
+static float integral; //"F32" 32bitの浮動小数点型
+
+int Line_in(int);                 // line_inの際の動作
+int Line_out(int);              // line_outの際の動作
+
+TASK(TaskMain)
+{
+  Clock clock;
+  Lcd lcd;
+
+  
+  int angle = 0;
+  
+  motorA.reset();
+  
+  while(touch.isPressed()==0)
+  {    
+    angle = pid_sample(light.getBrightness()); 
+    
+    //line_in 光センサの値がしきい値未満
+    if(light.getBrightness()<threshold){
+        Line_in(angle);
+        }
+    //line_out 光センサの値がしきい値以上
+    else{
+        Line_out(angle);
+        }
+
+    
+    lcd.clear();
+    //各センサ・モータ類の値を常に画面に出力
+    lcd.putf("sdn", "angle:", angle,0);
+    lcd.putf("sdn", "Rotation_A:", motorA.getCount(),0);
+    lcd.putf("sdn", "Rotation_B:", motorB.getCount(),0);
+    lcd.putf("sdn", "Rotation_C:", motorC.getCount(),0);
+    lcd.putf("sdn", "touch: ", touch.isPressed(),5);
+    lcd.putf("sdn", "gyro: ", gyro.getAnglerVelocity(),0);
+    lcd.putf("sdd",  "3/4: ", light.getBrightness(),0, sonar.getDistance(),5);
+    lcd.disp();
+
+    clock.wait(5);//msec_0.005
+  }
+}
+
+//line_inのとき左旋回
+//カラーセンサーの差分の値とflagの値で旋回角度を決める
+int Line_in(int angle){
+    if(motorA.getCount() > -angle){
+        motorA.setPWM(-100);
+        motorC.setPWM(-18);
+    }
+    else{
+      motorA.setPWM(0);
+      motorB.setPWM(-16);
+      motorC.setPWM(-18);
+    }
+}
+
+//line_outのとき右旋回
+int Line_out(int angle){
+    if(motorA.getCount() < angle){
+        motorA.setPWM(100);
+        motorB.setPWM(-18);
+    }
+    else{
+      motorA.setPWM(0);
+      motorB.setPWM(-18);
+      motorC.setPWM(-16);
+      }
+}
+
+
+float pid_sample(float sensor_val){
+    float p,i,d;
+    
+    diff[0]=diff[1];
+    diff[1] = sensor_val - target_val;
+    integral += (diff[1] + diff[0] / 2.0 * DELTA_T);
+    
+    p = KP * diff[1];
+    i = KI * integral;  
+    d = KD * (diff[1] - diff[0]) / DELTA_T;
+    
+    return math_limit(p + i + d, -100.0, 100.0);
+    }
+    
+float math_limit(float pid_val, float pid_min, float pid_max)
+{
+    if(pid_val < pid_min) {
+        return pid_min;
+    } else if(pid_val > pid_max) {
+        return pid_max;
+    }
+    return pid_val;
+}
+    
+}
+//end
